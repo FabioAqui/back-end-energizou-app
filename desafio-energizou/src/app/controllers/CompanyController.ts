a9ecfc41070da839fdc1a35f11182904c625adcc
FabioAqui <fabiooito9@gmail.com>
2023-11-05 18:28:17 -0300

    Segundo commi crud completo

--- desafio-energizou/src/app/controllers/CompanyController.ts
+++ desafio-energizou/src/app/controllers/CompanyController.ts
@@ -10,4 +10,28 @@ companyRouter.get('/', async (_req: Request, res: Response): Promise<Response> =
     return res.status(200).json(companies);
 });
 
+companyRouter.post('/', async (req: Request, res: Response): Promise<Response> => {
+    const companies = await CompanyRepository.newCompany(req.body);
+    return res.status(200).json(companies);
+});
+
+companyRouter.get('/:id', async (req: Request, res: Response): Promise<Response> => {
+    const id = parseInt(req.params.id);
+    const companies = await CompanyRepository.getCompanyById(id);
+    return res.status(200).json(companies);
+});
+
+companyRouter.put('/:id', async (req: Request, res: Response): Promise<Response> => {
+    const id = parseInt(req.params.id);
+    const company = await CompanyRepository.updateCompany(id, req.body);
+    return res.status(201).json(company);
+});
+
+companyRouter.delete('/:id', async (req: Request, res: Response): Promise<Response> => {
+    const id = parseInt(req.params.id);
+    const companies = await CompanyRepository.deleteCompany(id);
+    return res.status(201).json({message:"Empresa removida com sucesso!"});
+});
+
+
 export default companyRouter;
--- desafio-energizou/src/app/repositories/CompanyRepository.ts
+++ desafio-energizou/src/app/repositories/CompanyRepository.ts
@@ -1,6 +1,7 @@
 import Company from "../entities/Company";
 import ICompany from "../interfaces/ICompany";
 import { AppDataSource } from "../../database/data-source";
+import { error } from "console";
 
 const companyRepository = AppDataSource.getRepository(Company);
 
@@ -8,4 +9,39 @@ const getCompanies = (): Promise<ICompany[]> => {
     return companyRepository.find();
 }
 
-export default { getCompanies };
+const getCompanyById = async (idt: number): Promise<ICompany | undefined> => {
+    try {
+        const company = await companyRepository.findOne({where: {id: idt}});
+    
+        if (!company){
+            throw new Error("Empresa não encontrada");
+        };
+    
+        return company;
+      } catch (error) {
+        throw error;
+      }
+}
+
+
+const deleteCompany = (id: number): Promise<void> => {
+    return companyRepository.delete(id).then(() => {});
+}
+
+const updateCompany = (idt: number, updatedCompanyData: Partial<ICompany>): Promise<ICompany | undefined> => {
+    return companyRepository
+    .findOne({where: {id: idt}})
+    .then((company) => {
+        if (company) {
+          companyRepository.merge(company, updatedCompanyData);
+          return companyRepository.save(company);
+        }
+        return undefined;
+    });
+}
+const newCompany = (newCompanyData: ICompany): Promise<ICompany> => {
+    const newCompany = companyRepository.create(newCompanyData);
+    return companyRepository.save(newCompany);
+}
+
+export default { getCompanies, getCompanyById, deleteCompany, newCompany, updateCompany };
